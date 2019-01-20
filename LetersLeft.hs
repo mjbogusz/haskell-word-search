2c09a61a8de113592971923c664ea2f832ea3f40
Tomasz Jakubczyk <sigrond93@gmail.com>
2019-01-20 21:46:02 +0100

    pozostawianie niewykreślonych liter

---
+++ LetersLeft.hs
@@ -0,0 +1,13 @@
+module LetersLeft where
+
+import Data.List
+
+letersLeft :: [String] -> [[Bool]] -> [String]
+letersLeft (mapline:map) (flagsline:flags) = [letersLeftLine mapline flagsline] ++ (letersLeft map flags)
+
+letersLeftLine :: String -> [Bool] -> String
+letersLeftLine [] [] = [];
+letersLeftLine (leter:mapline) (flag:flagsline) = (if flag == True
+                                                  then "X"
+                                                  else [leter])
+                                              ++ letersLeftLine mapline flagsline
